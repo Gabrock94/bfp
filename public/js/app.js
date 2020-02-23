5b1d2f47dcbb2934de6add06215c5865a4b14423
Giulio <gack94@gmail.com>
2020-02-23 15:05:39 +0800

    Added support for firestore caching
    Firestore snapshots are now stored offline and available for offline usage.

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,11 @@ function createAlert(alertType, descriptionn){
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker.register("/serviceWorker.js").then(console.log("service worker registered")).catch(err => console.log("service worker not registered", err));
-    document.body.innerHTML = createAlert('success','Hey! You can now use this app offline!') + document.body.innerHTML;
+    online = window.navigator.onLine;
+    if(online){
+        document.body.innerHTML = createAlert('success','Hey! You can now use this app offline!') + document.body.innerHTML;
+    }else{
+        document.body.innerHTML = createAlert('info','Hey! You are currently using an offline version of this app') + document.body.innerHTML;
+    }
   })
 }
--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,4 @@
-const bpf = "bfp_0.0.1.4";
+const BFP = "bfp_0.0.1.9";
 
 const assets = [
     "/",
@@ -11,38 +11,33 @@ const assets = [
     "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
     "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
     "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js",
-    "https://www.w3schools.com/lib/w3.js"
+    "/__/firebase/7.8.2/firebase-firestore.js"
 ]
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(bfp).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    console.log('installing sw');
+    event.waitUntil(
+        caches.open(BFP)
+            .then(function(cache) {
+                console.log('Opened cache');
+                var x = cache.addAll(assets);
+                console.log('cache added');
+                return x;
+            })
+    );
+});
 
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
-  )
-})
-
-self.addEventListener('activate', function(event) {
-
-  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
 });
